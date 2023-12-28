36b805d3a7caff82493b95c02d3bd9db93697a2b
Om Ganjale <ganjaleom14@gmail.com>
2023-12-28 18:55:49 +0530

    git upload

---
+++ day 4/chapter1 booleans if-else/script.js
@@ -0,0 +1,19 @@
+console.log(5>5);
+console.log(5<8);
+console.log(5===8);
+console.log(5==8);
+console.log(5>=5);
+console.log(5<=5);
+
+
+let omi = 'king';
+
+if(omi=='king'){
+    console.log('kakaji');
+}
+else if(omi=='kingo'){
+    console.log('omiji');
+}
+else{
+    console.log('pappaji');
+}
